13716419e2822886156695e2e98a89e62f584774
VuLT <vult@azuraglobal.app>
2025-03-28 23:23:57 +0700

    up version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "net.blwsmartware.qrcodescanner"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val formattedDate = SimpleDateFormat("MMM.dd.yyyy").format(Date())
         base.archivesName = "FileRecovery-v$versionName($versionCode)_${formattedDate}"
